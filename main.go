ea01b95689c26d88e8fe80dacdc7022f29e69df6
Williamrobert Green <wkgreen13@gmail.com>
2019-05-28 15:47:15 -0400

    makes initial connection to update status

--- main.go
+++ main.go
@@ -7,11 +7,36 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-func main() {
-	bot, err := discordgo.New("Bot " + os.Getenv("DISCORD_TOKEN"))
-	if err != nil {
-		fmt.Println("ERROR connecting to discord:", err)
-	}
+var botID string
+var commandPrefix string
 
-	fmt.Println("connected with", bot.Token)
+func main() {
+	session, err := discordgo.New("Bot " + os.Getenv("DISCORD_TOKEN"))
+	errCheck("could not connect to discord:", err)
+
+	botID, err := session.User("@me")
+	errCheck("ERROR could not get user info", err)
+
+	fmt.Println("Logged in as", botID)
+
+	session.AddHandler(onReady)
+
+	err = session.Open()
+	errCheck("could not open connection to Discord", err)
+	defer session.Close()
+	defer session.UpdateStatus(1, "")
+
+	commandPrefix = "!"
+
+	<-make(chan struct{})
+}
+
+func errCheck(message string, err error) {
+	if err != nil {
+		fmt.Println("ERROR", message, err)
+	}
+}
+
+func onReady(session *discordgo.Session, ready *discordgo.Ready) {
+	session.UpdateStatus(0, "Introduction to Basics")
 }
